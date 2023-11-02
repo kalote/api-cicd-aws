c3947b70d1c38192380cbdd825174a5380edec46
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-11-02 15:10:51 +0100

    appliction init + test setup

---
+++ app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response, Application } from "express";
+import bodyParser from "body-parser";
+
+const app: Application = express();
+const parser = bodyParser.json();
+
+// home
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    "<h1>Welcome 🫡.</h1><h4>Here is a list of endpoints:</h4><br />- '/status' to get application status 🩺<br />- '/data' to send data 👨‍💻."
+  );
+});
+
+// health check
+app.get("/status", (req: Request, res: Response) => {
+  res.json({ message: "ok" });
+});
+
+app.post("/data", parser, (req: Request, res: Response) => {
+  if (!req.body.name)
+    return res
+      .status(400)
+      .json({ status: "400", message: "missing 'name' property" });
+
+  if (!req.body.position)
+    return res
+      .status(400)
+      .json({ status: "400", message: "missing 'position' property" });
+
+  const { name, position } = req.body;
+  return res.json({ name, position });
+});
+
+export default app;
---
+++ server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
