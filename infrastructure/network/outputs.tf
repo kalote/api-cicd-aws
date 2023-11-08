7895d5cf2de16053976a0399673146d3e725b51f
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-11-08 09:52:41 +0100

    fix output issues

--- infrastructure/network/outputs.tf
+++ infrastructure/network/outputs.tf
@@ -3,22 +3,12 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "subnet1_id" {
-  description = "The subnet 1 ID"
-  value       = aws_subnet.subnet1.id
+output "subnet_public_id" {
+  description = "The public subnet ID"
+  value       = aws_subnet.subnet_public.id
 }
 
-output "subnet2_id" {
-  description = "The subnet 2 ID"
-  value       = aws_subnet.subnet2.id
-}
-
-output "sg_app_id" {
-  description = "The app security group ID"
-  value       = aws_security_group.security_group_app.id
-}
-
-output "sg_mongo_id" {
-  description = "The mongo security group ID"
-  value       = aws_security_group.security_group_mongo.id
+output "subnet_private_id" {
+  description = "The private subnet ID"
+  value       = aws_subnet.subnet_private.id
 }
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -13,37 +13,17 @@ output "vpc_id" {
   value       = module.network.vpc_id
 }
 
-output "subnet1_id" {
-  description = "The subnet 1 ID"
-  value       = module.network.subnet1_id
+output "subnet_public_id" {
+  description = "The public subnet ID"
+  value       = module.network.subnet_public_id
 }
 
-output "subnet2_id" {
-  description = "The subnet 2 ID"
-  value       = module.network.subnet2_id
+output "subnet_private_id" {
+  description = "The private subnet ID"
+  value       = module.network.subnet_private_id
 }
-
-output "sg_app_id" {
-  description = "The app security group ID"
-  value       = module.network.sg_app_id
-}
-
-output "sg_mongo_id" {
-  description = "The mongo security group ID"
-  value       = module.network.sg_mongo_id
-}
-
-# output "alb_dns_name" {
-#   description = "The LB DNS name"
-#   value       = module.instances.alb_dns_name
-# }
 
 output "ecr_registry_url" {
   description = "The ECR registry URL"
   value       = module.ecr.repository_url
 }
-
-# output "target_group_arn" {
-#   description = "The target group ARN"
-#   value       = module.instances.target_group_arn
-# }
