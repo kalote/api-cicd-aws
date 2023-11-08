e76048a4113fd817df56eeb058acb025c0581790
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-11-08 11:54:38 +0100

    debug init script

--- infrastructure/instances/templates/init-application.sh
+++ infrastructure/instances/templates/init-application.sh
@@ -3,5 +3,7 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin ${registry_id}.dkr.ecr.${registry_region}.amazonaws.com
+echo "aws ecr / docker login"
+sudo aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin ${registry_id}.dkr.ecr.${registry_region}.amazonaws.com
+echo "docker run"
 sudo docker run --name mynginx1 -p 80:8000 -d ${repo_url}:${app_version}
