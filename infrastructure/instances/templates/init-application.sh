c22a8a3114c89664ffabd429451033e93f4822e5
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-11-08 14:32:22 +0100

    connecting apps

--- infrastructure/instances/templates/init-application.sh
+++ infrastructure/instances/templates/init-application.sh
@@ -6,4 +6,6 @@ sudo usermod -a -G docker ec2-user
 aws ecr get-login-password --region ${registry_region} | docker login \
     --username AWS \
     --password-stdin https://${registry_id}.dkr.ecr.${registry_region}.amazonaws.com
-sudo docker run --name api-app -p 80:8000 -d ${repo_url}:${app_version}
+sudo docker run --name api-app -p 80:8000 -d ${repo_url}:${app_version} \
+    -e REDIS_URL="http://${redis_instance_ip}/0" \
+    -e DB_URL="mongodb://admin:mypassword@${mongo_instance_ip}:27017/appdb?authSource=admin"
