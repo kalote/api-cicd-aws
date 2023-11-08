f820fa7cd2eb0e5dc17fadf9a414888f60f7793d
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-11-08 16:57:44 +0100

    feat: adding secret

--- infrastructure/instances/data.tf
+++ infrastructure/instances/data.tf
@@ -43,3 +43,21 @@ data "aws_iam_policy_document" "ecr_access" {
     ]
   }
 }
+
+data "aws_secretsmanager_secret" "env_secrets" {
+  name = "dev/db_credentials"
+}
+
+data "aws_iam_policy_document" "secret_access" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+
+    resources = [
+      data.aws_secretsmanager_secret.env_secrets.arn
+    ]
+
+    effect = "Allow"
+  }
+}
--- infrastructure/instances/templates/init-application.sh
+++ infrastructure/instances/templates/init-application.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 sudo yum update -y
-sudo yum install -y docker
+sudo yum install -y docker jq
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+cred=$(aws secretsmanager get-secret-value --region ${registry_region} \
+    --secret-id ${secret_name} --query SecretString \
+    --output text | jq -r .DB_CREDENTIALS)
+aws secretsmanager get-secret-value --region ${registry_region} --secret-id ${secret_name}
 aws ecr get-login-password --region ${registry_region} | docker login \
     --username AWS \
     --password-stdin https://${registry_id}.dkr.ecr.${registry_region}.amazonaws.com
 sudo docker run -d \
     -e REDIS_URL="redis://${redis_instance_ip}:6379/0" \
-    -e DB_URL="mongodb://admin:mypassword@${mongo_instance_ip}:27017/appdb?authSource=admin" \
+    -e DB_URL="mongodb://$cred@${mongo_instance_ip}:27017/appdb?authSource=admin" \
     -p 80:8000 \
     --name api-app ${repo_url}:${app_version}
