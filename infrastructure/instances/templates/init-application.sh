fe4abe43722e3f3ceed730ace1fe08637ee7ba5f
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-11-08 12:26:15 +0100

    changes on app init script

--- infrastructure/instances/templates/init-application.sh
+++ infrastructure/instances/templates/init-application.sh
@@ -4,6 +4,6 @@ sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 echo "aws ecr / docker login"
-sudo aws ecr get-login-password --region ${registry_region} | docker login --username AWS --password-stdin ${registry_id}.dkr.ecr.${registry_region}.amazonaws.com
+aws ecr get-login-password --region ${registry_region} | docker login --username AWS --password-stdin ${registry_id}.dkr.ecr.${registry_region}.amazonaws.com
 echo "docker run"
-sudo docker run -p 80:8000 -d ${repo_url}:${app_version}
+docker run -p 80:8000 -d ${repo_url}:${app_version}
